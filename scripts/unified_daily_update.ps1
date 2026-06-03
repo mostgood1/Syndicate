9f6710e0c4b11c51ee949ab7035fa4ff2a4dd14c
mostgood1 <mostgood@gmail.com>
2026-06-03 18:23:49 -0500

    daily update 2026-06-03 (pre-source publish)

--- scripts/unified_daily_update.ps1
+++ scripts/unified_daily_update.ps1
@@ -1446,7 +1446,6 @@ if (-not $SkipNBA) {
     $nbaEnvOverrides = @{
         REFRESH_PREDICT_PROPS_SMART_SIM_N_SIMS = [string]$runtimePolicy.NBA.smartsimNSims
         REFRESH_PREDICT_PROPS_SMART_SIM_WORKERS = [string]$runtimePolicy.NBA.smartsimWorkers
-        SYNDICATE_NBA_SOURCE_APP_FALLBACK = '1'
     }
     if (-not [string]::IsNullOrWhiteSpace($sharedOddsApiKey)) {
         $nbaEnvOverrides.ODDS_API_KEY = $sharedOddsApiKey
@@ -1483,7 +1482,6 @@ if (-not $SkipWNBA) {
     $wnbaEnvOverrides = @{
         REFRESH_PREDICT_PROPS_SMART_SIM_N_SIMS = [string]$runtimePolicy.WNBA.smartsimNSims
         REFRESH_PREDICT_PROPS_SMART_SIM_WORKERS = [string]$runtimePolicy.WNBA.smartsimWorkers
-        SYNDICATE_WNBA_SOURCE_APP_FALLBACK = '1'
     }
     if (-not [string]::IsNullOrWhiteSpace($sharedOddsApiKey)) {
         $wnbaEnvOverrides.ODDS_API_KEY = $sharedOddsApiKey
